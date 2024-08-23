49bd9ed1e63f84bca67b3d0aba5381d8746829d7
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-24 07:40:29 +0900

    test: initial cli output and return code

---
+++ tests/test_cli.py
@@ -0,0 +1,8 @@
+import subprocess
+
+
+class TestCli:
+    def test_with_no_option(self) -> None:
+        result = subprocess.run(['chronovoyage'], capture_output=True)
+        result.check_returncode()
+        assert result.stdout.rstrip() == b"Hello world!"
