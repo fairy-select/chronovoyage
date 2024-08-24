5d051106ab46c8786883675e9856b69f54ba3596
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-24 10:56:04 +0900

    test: mariadb コネクション取得処理をヘルパーに移動

--- tests/chronovoyage/database/test_connection.py
+++ tests/chronovoyage/database/test_connection.py
@@ -1,19 +1,4 @@
-import sys
-
-import mariadb
-
-from helper import DEFAULT_TEST_ENV
-
-
 class TestConnection:
-    def test_connect_to_mariadb(self) -> None:
-        try:
-            conn = mariadb.connect(
-                user=DEFAULT_TEST_ENV["MARIADB_USER"],
-                password=DEFAULT_TEST_ENV["MARIADB_PASSWORD"],
-                host="127.0.0.1",
-                port=3307,
-            )
-        except mariadb.Error as e:
-            print(f"Error connecting to MariaDB Platform: {e}")
-            sys.exit(1)
+    def test_connect_to_mariadb(self, database_helper) -> None:
+        cnx = database_helper.get_connection('mariadb')
+        assert cnx is not None
