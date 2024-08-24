2db4f0c5dfc421d83277449f474b67edca9a2864
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-24 11:22:30 +0900

    test: あらかじめ想定していない DB ベンダーを使おうとしたらエラーにする

--- tests/chronovoyage/database/test_connection.py
+++ tests/chronovoyage/database/test_connection.py
@@ -1,4 +1,18 @@
+import pytest
+
+from chronovoyage.database.connection import DatabaseConnector, ConnectionInfo
+from chronovoyage.exception.database import DatabaseUnknownVendorError
+
+
 class TestConnection:
     def test_connect_to_mariadb(self, database_helper) -> None:
         cnx = database_helper.get_connection('mariadb')
         assert cnx is not None
+
+    def test_connect_to_unknown(self) -> None:
+        # given
+        connection_info = ConnectionInfo(user="", password="", host="", port=0)
+        # when/then
+        with pytest.raises(DatabaseUnknownVendorError):
+            # noinspection PyTypeChecker
+            DatabaseConnector().get_connection('unknown', connection_info)
