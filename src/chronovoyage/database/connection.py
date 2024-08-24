ec47c15d0c1b060f69f2f1eac67ac6f5fed2bf3c
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-24 11:10:25 +0900

    feat: DB コネクション取得処理を src に移動

---
+++ src/chronovoyage/database/connection.py
@@ -0,0 +1,33 @@
+import sys
+from dataclasses import dataclass
+from typing import Literal
+
+DatabaseVendor = Literal["mariadb"]
+
+
+@dataclass(frozen=True)
+class ConnectionInfo:
+    user: str
+    password: str
+    host: str
+    port: int
+
+class DatabaseConnector:
+    # noinspection PyMethodMayBeStatic
+    def get_connection(self, vendor: DatabaseVendor, connection_info: ConnectionInfo):
+        if vendor == "mariadb":
+            import mariadb
+            try:
+                conn = mariadb.connect(
+                    user=connection_info.user,
+                    password=connection_info.password,
+                    host=connection_info.host,
+                    port=connection_info.port,
+                )
+            except mariadb.Error as e:
+                print(f"Error connecting to MariaDB Platform: {e}")
+                sys.exit(1)
+
+            return conn
+
+        return None
--- tests/helper/database.py
+++ tests/helper/database.py
@@ -1,27 +1,16 @@
-import sys
-from typing import Literal
-
+from chronovoyage.database.connection import DatabaseVendor, DatabaseConnector, ConnectionInfo
 from helper import DEFAULT_TEST_ENV
-
-DatabaseVendor = Literal["mariadb"]
 
 
 class DatabaseHelper:
     # noinspection PyMethodMayBeStatic
     def get_connection(self, vendor: DatabaseVendor):
+        database_connector = DatabaseConnector()
         if vendor == "mariadb":
-            import mariadb
-            try:
-                conn = mariadb.connect(
-                    user=DEFAULT_TEST_ENV["MARIADB_USER"],
-                    password=DEFAULT_TEST_ENV["MARIADB_PASSWORD"],
-                    host="127.0.0.1",
-                    port=3307,
-                )
-            except mariadb.Error as e:
-                print(f"Error connecting to MariaDB Platform: {e}")
-                sys.exit(1)
-
-            return conn
-
-        return None
+            connection_info = ConnectionInfo(
+                user=DEFAULT_TEST_ENV["MARIADB_USER"],
+                password=DEFAULT_TEST_ENV["MARIADB_PASSWORD"],
+                host="127.0.0.1",
+                port=3307,
+            )
+            return database_connector.get_connection('mariadb', connection_info)
