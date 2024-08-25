7366b2883eed0f2688e4243b9a36dc22f625e454
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-25 16:49:36 +0900

    test: move variable

--- tests/chronovoyage/domain/test_migrate.py
+++ tests/chronovoyage/domain/test_migrate.py
@@ -5,16 +5,15 @@ from chronovoyage.config.migrate import MigrateDomainConfigFactory
 from chronovoyage.domain.migrate import MigrateDomain
 from chronovoyage.logger import get_default_logger
 
-logger = get_default_logger()
-
 
 class TestMigrateDomain:
     @pytest.fixture(autouse=True)
     def _(self) -> None:
+        self.logger = get_default_logger()
         self.config_factory = MigrateDomainConfigFactory()
 
     def test_migrate_ddl_only_create_db(self) -> None:
         migrate_domain_config = self.config_factory.create_from_directory(
             f"{TESTS_DIR}/resource/mariadb/ddl_only_create_db"
         )
-        MigrateDomain(migrate_domain_config, logger=logger).execute()
+        MigrateDomain(migrate_domain_config, logger=self.logger).execute()
