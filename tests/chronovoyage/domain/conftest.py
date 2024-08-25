3d0f2c1e10d115ac10cbe4e8a1e26f21b89130d3
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-25 21:13:41 +0900

    test: add docs

--- tests/chronovoyage/domain/conftest.py
+++ tests/chronovoyage/domain/conftest.py
@@ -7,5 +7,6 @@ from helper import RESOURCE_DIR
 
 @pytest.fixture
 def mariadb_resource_dir(request: SubRequest) -> str:
+    """テストケース名に対応した mariadb の設定ディレクトリを取得する"""
     test_name = re.match(r"^test_(?P<test_name>.+)$", request.node.name).group("test_name")
     return f"{RESOURCE_DIR}/mariadb/{test_name}"
