fdf5d436289d33942bbc49ca2584e72bfeb07f1c
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-25 18:40:19 +0900

    test: detect config dir with fixture

---
+++ tests/chronovoyage/domain/conftest.py
@@ -0,0 +1,12 @@
+import re
+
+import pytest
+from _pytest.fixtures import SubRequest
+
+from helper import RESOURCE_DIR
+
+
+@pytest.fixture
+def mariadb_resource_dir(request: SubRequest) -> str:
+    test_name = re.match(r"^test_(?P<test_name>.+)$", request.node.name).group("test_name")
+    return f"{RESOURCE_DIR}/mariadb/{test_name}"
--- tests/chronovoyage/domain/test_migrate.py
+++ tests/chronovoyage/domain/test_migrate.py
@@ -1,5 +1,4 @@
 import pytest
-from helper import RESOURCE_DIR
 
 from chronovoyage.config.migrate import MigrateDomainConfigFactory
 from chronovoyage.domain.migrate import MigrateDomain
@@ -18,27 +17,22 @@ class TestMigrateDomainMariadb:
         self.logger = get_default_logger()
         self.config_factory = MigrateDomainConfigFactory()
 
-    def test_ddl_only(self) -> None:
-        config_dir = f"{RESOURCE_DIR}/mariadb/ddl_only"
-        migrate_domain_config = self.config_factory.create_from_directory(config_dir)
+    def test_ddl_only(self, mariadb_resource_dir) -> None:
+        migrate_domain_config = self.config_factory.create_from_directory(mariadb_resource_dir)
         MigrateDomain(migrate_domain_config, logger=self.logger).execute()
 
-    def test_period_name_invalid(self) -> None:
-        config_dir = f"{RESOURCE_DIR}/mariadb/period_name_invalid"
+    def test_period_name_invalid(self, mariadb_resource_dir) -> None:
         with pytest.raises(MigrateConfigVersionNameInvalidError):
-            _ = self.config_factory.create_from_directory(config_dir)
+            _ = self.config_factory.create_from_directory(mariadb_resource_dir)
 
-    def test_both_sqls_missing(self) -> None:
-        config_dir = f"{RESOURCE_DIR}/mariadb/both_sqls_missing"
+    def test_both_sqls_missing(self, mariadb_resource_dir) -> None:
         with pytest.raises(MigrateConfigSqlMissingError):
-            _ = self.config_factory.create_from_directory(config_dir)
+            _ = self.config_factory.create_from_directory(mariadb_resource_dir)
 
-    def test_go_sql_missing(self) -> None:
-        config_dir = f"{RESOURCE_DIR}/mariadb/go_sql_missing"
+    def test_go_sql_missing(self, mariadb_resource_dir) -> None:
         with pytest.raises(MigrateConfigGoSqlMissingError):
-            _ = self.config_factory.create_from_directory(config_dir)
+            _ = self.config_factory.create_from_directory(mariadb_resource_dir)
 
-    def test_return_sql_missing(self) -> None:
-        config_dir = f"{RESOURCE_DIR}/mariadb/return_sql_missing"
+    def test_return_sql_missing(self, mariadb_resource_dir) -> None:
         with pytest.raises(MigrateConfigReturnSqlMissingError):
-            _ = self.config_factory.create_from_directory(config_dir)
+            _ = self.config_factory.create_from_directory(mariadb_resource_dir)
