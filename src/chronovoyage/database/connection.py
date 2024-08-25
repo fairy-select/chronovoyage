b6bb7fc3c6157e1776280f448f19bc275f0d628b
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-25 16:07:24 +0900

    refactor: mariadb 接続処理を別ファイルに切り出す

--- src/chronovoyage/database/connection.py
+++ src/chronovoyage/database/connection.py
@@ -28,50 +28,7 @@ class DatabaseConnector:
     # noinspection PyMethodMayBeStatic
     def get_connection(self, vendor: DatabaseVendorEnum, connection_info: ConnectionInfo):
         if vendor == "mariadb":
-            import mariadb
-
-            try:
-                conn = mariadb.connect(
-                    user=connection_info.user,
-                    password=connection_info.password,
-                    host=connection_info.host,
-                    port=connection_info.port,
-                )
-            except mariadb.Error:
-                self._logger.exception("Error connecting to MariaDB Platform")
-                sys.exit(1)
-
-            class MariadbDatabaseTransaction:
-                def __init__(self, _conn: mariadb.Connection) -> None:
-                    self._conn = conn
-
-                def __enter__(self) -> mariadb.Connection:
-                    self._conn.begin()
-                    return self._conn
-
-                def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-                    if exc_type is None:
-                        self._conn.commit()
-                    else:
-                        self._conn.rollback()
-
-            class MariadbDatabaseConnectionWrapper:
-                def __init__(self, _conn: mariadb.Connection) -> None:
-                    self._conn = conn
-
-                def begin(self) -> MariadbDatabaseTransaction:
-                    return MariadbDatabaseTransaction(self._conn)
-
-            class MariadbDatabaseConnection:
-                def __init__(self, _conn: mariadb.Connection) -> None:
-                    self._conn = conn
-
-                def __enter__(self) -> MariadbDatabaseConnectionWrapper:
-                    return MariadbDatabaseConnectionWrapper(self._conn)
-
-                def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-                    self._conn.close()
-
-            return MariadbDatabaseConnection(conn)
+            from chronovoyage.database import mariadb
+            return mariadb.connect(connection_info, logger=self._logger)
 
         raise DatabaseUnknownVendorError
---
+++ src/chronovoyage/database/mariadb.py
@@ -0,0 +1,52 @@
+import sys
+from logging import Logger
+
+from chronovoyage.database.connection import ConnectionInfo
+
+
+def connect(connection_info: ConnectionInfo, *, logger: Logger):
+    import mariadb
+
+    try:
+        conn = mariadb.connect(
+            user=connection_info.user,
+            password=connection_info.password,
+            host=connection_info.host,
+            port=connection_info.port,
+        )
+    except mariadb.Error:
+        logger.exception("Error connecting to MariaDB Platform")
+        sys.exit(1)
+
+    class MariadbDatabaseTransaction:
+        def __init__(self, _conn: mariadb.Connection) -> None:
+            self._conn = conn
+
+        def __enter__(self) -> mariadb.Connection:
+            self._conn.begin()
+            return self._conn
+
+        def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+            if exc_type is None:
+                self._conn.commit()
+            else:
+                self._conn.rollback()
+
+    class MariadbDatabaseConnectionWrapper:
+        def __init__(self, _conn: mariadb.Connection) -> None:
+            self._conn = conn
+
+        def begin(self) -> MariadbDatabaseTransaction:
+            return MariadbDatabaseTransaction(self._conn)
+
+    class MariadbDatabaseConnection:
+        def __init__(self, _conn: mariadb.Connection) -> None:
+            self._conn = conn
+
+        def __enter__(self) -> MariadbDatabaseConnectionWrapper:
+            return MariadbDatabaseConnectionWrapper(self._conn)
+
+        def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+            self._conn.close()
+
+    return MariadbDatabaseConnection(conn)
