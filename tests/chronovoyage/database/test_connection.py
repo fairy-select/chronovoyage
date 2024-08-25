e611e9189470617d9fb4eeb642222d7458733803
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-25 18:56:05 +0900

    test: remove helper/database.py

--- tests/chronovoyage/database/test_connection.py
+++ tests/chronovoyage/database/test_connection.py
@@ -3,12 +3,23 @@ import pytest
 from chronovoyage.internal.database.connection import ConnectionInfo, DatabaseConnector
 from chronovoyage.internal.exception.database import DatabaseUnknownVendorError
 from chronovoyage.internal.type.enum import DatabaseVendorEnum
+from helper import DEFAULT_TEST_ENV
 
 
 class TestConnection:
-    def test_connect_to_mariadb(self, database_helper) -> None:
-        cnx = database_helper.get_connection(DatabaseVendorEnum.MARIADB)
-        assert cnx is not None
+    def test_connect_to_mariadb(self) -> None:
+        # given
+        connection_info = ConnectionInfo(
+                host="127.0.0.1",
+                port=3307,
+                user=DEFAULT_TEST_ENV["MARIADB_USER"],
+                password=DEFAULT_TEST_ENV["MARIADB_PASSWORD"],
+                database=DEFAULT_TEST_ENV["MARIADB_DATABASE"],
+        )
+        # when
+        conn = DatabaseConnector().get_connection(DatabaseVendorEnum.MARIADB, connection_info)
+        # then
+        assert conn is not None
 
     def test_connect_to_unknown(self) -> None:
         # given
--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from helper.database import DatabaseHelper
-
-
-@pytest.fixture
-def database_helper() -> DatabaseHelper:
-    return DatabaseHelper()
