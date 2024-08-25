dc20fe3611d6180ec60173264bb2f6655aa4cade
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-08-25 16:51:18 +0900

    test: introduce RESOURCE_DIR

--- tests/chronovoyage/domain/test_migrate.py
+++ tests/chronovoyage/domain/test_migrate.py
@@ -1,9 +1,9 @@
 import pytest
-from helper import TESTS_DIR
 
 from chronovoyage.config.migrate import MigrateDomainConfigFactory
 from chronovoyage.domain.migrate import MigrateDomain
 from chronovoyage.logger import get_default_logger
+from helper import RESOURCE_DIR
 
 
 class TestMigrateDomainMariadb:
@@ -14,6 +14,6 @@ class TestMigrateDomainMariadb:
 
     def test_migrate_ddl_only_create_db(self) -> None:
         migrate_domain_config = self.config_factory.create_from_directory(
-            f"{TESTS_DIR}/resource/mariadb/ddl_only_create_db"
+            f"{RESOURCE_DIR}/mariadb/ddl_only_create_db"
         )
         MigrateDomain(migrate_domain_config, logger=self.logger).execute()
--- tests/helper/__init__.py
+++ tests/helper/__init__.py
@@ -3,4 +3,5 @@ import os
 from dotenv import dotenv_values
 
 TESTS_DIR = os.path.realpath(f"{os.path.dirname(__file__)}/..")
+RESOURCE_DIR = f"{TESTS_DIR}/resource"
 DEFAULT_TEST_ENV = dotenv_values(f"{TESTS_DIR}/test.env")
