d5fbe82b580971b33f514b816bb9a6c538085090
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-09-12 21:10:46 +0900

    test: wip - test_partially_migrate

--- tests/chronovoyage/domain/test_migrate.py
+++ tests/chronovoyage/domain/test_migrate.py
@@ -33,6 +33,17 @@ class TestMigrateDomainMariadb:
             cursor.execute("SELECT has_come FROM chronovoyage_periods")
             return {has_come for (has_come,) in cursor.fetchall()} == {True}
 
+    @property
+    def _current_period(self) -> str | None:
+        with get_default_mariadb_connection() as wrapper:
+            cursor = wrapper.cursor()
+            cursor.execute("SELECT period_name FROM chronovoyage_periods WHERE has_come IS TRUE ORDER BY id DESC")
+            row = cursor.fetchone()
+            if row is None:
+                return None
+            (period_name,) = row
+            return period_name
+
     def test_ddl_only(self, mariadb_migrate_domain_config) -> None:
         # when
         MigrateDomain(mariadb_migrate_domain_config, logger=self.logger).execute()
@@ -67,5 +78,23 @@ class TestMigrateDomainMariadb:
             cursor = wrapper.cursor()
             # noinspection SqlResolve
             cursor.execute("SELECT * FROM user ORDER BY id")
-            for i, (got_user, want_user) in enumerate(zip(cursor, want_users)):
-                assert got_user == want_user, f"row {i}"
+            if cursor.rowcount != len(want_users):
+                pytest.fail(f"件数が異なる (want: {len(want_users)}), got: {cursor.rowcount}")
+            for i, got_user in enumerate(cursor):
+                assert got_user == want_users[i], f"row {i}"
+
+    @pytest.mark.skip
+    def test_partially_migrate(self, mariadb_migrate_domain_config) -> None:
+        # when
+        MigrateDomain(mariadb_migrate_domain_config, logger=self.logger).execute()
+        # then
+        assert self._current_period == "19991231235902"
+        want_users = [(1, 'Jane'), (2, 'John')]
+        with get_default_mariadb_connection() as wrapper:
+            cursor = wrapper.cursor()
+            # noinspection SqlResolve
+            cursor.execute("SELECT * FROM user ORDER BY id")
+            if cursor.rowcount != len(want_users):
+                pytest.fail(f"件数が異なる (want: {len(want_users)}), got: {cursor.rowcount}")
+            for i, got_user in enumerate(cursor):
+                assert got_user == want_users[i], f"row {i}"
