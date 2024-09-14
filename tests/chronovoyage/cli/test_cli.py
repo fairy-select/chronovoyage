497a172292bf036bc184b1cb1044a102ddf7554b
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-09-14 10:04:02 +0900

    test: rename testcase

--- tests/chronovoyage/cli/test_cli.py
+++ tests/chronovoyage/cli/test_cli.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 class TestCli:
-    def test_with_no_command_and_no_option(self) -> None:
+    def test_with_no_command_and_no_options(self) -> None:
         result = subprocess.run(["chronovoyage"], capture_output=True, check=True)
         assert result.stdout.startswith(b"Usage:"), "show help"
 
