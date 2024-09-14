1b48c92f3862f3a7b4b82bd75ec9125b9d10e7a7
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-09-14 10:11:22 +0900

    test: parametrize

--- tests/chronovoyage/cli/test_cli.py
+++ tests/chronovoyage/cli/test_cli.py
@@ -1,11 +1,19 @@
 import subprocess
 
+import pytest
+
 
 class TestCli:
     def test_with_no_command_and_no_options(self) -> None:
         result = subprocess.run(["chronovoyage"], capture_output=True, check=True)
         assert result.stdout.startswith(b"Usage:"), "show help"
 
-    def test_migrate_with_no_options(self) -> None:
-        result = subprocess.run(["chronovoyage", "migrate"], capture_output=True, check=False)
+    @pytest.mark.parametrize(
+        "command",
+        [
+            pytest.param("migrate"),
+        ],
+    )
+    def test_migrate_with_no_options(self, command: str) -> None:
+        result = subprocess.run(["chronovoyage", command], capture_output=True, check=False)
         assert result.stderr.startswith(b"Usage:"), "show help"
