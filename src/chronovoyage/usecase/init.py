11bf6155e9a96fd42b7d11e329986c23673fcd49
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-09-14 12:10:17 +0900

    feat: create initial directory

--- src/chronovoyage/usecase/init.py
+++ src/chronovoyage/usecase/init.py
@@ -1,3 +1,5 @@
+import json
+import os
 from logging import Logger
 
 
@@ -6,4 +8,18 @@ class InitUsecase:
         self._logger = logger
 
     def create_files(self, *, to_directory: str) -> None:
-        pass
+        os.makedirs(to_directory, exist_ok=True)
+        config = {
+            "$schema": "https://raw.githubusercontent.com/noritakaIzumi/chronovoyage/main/schema/config.schema.json",
+            "vendor": "mariadb",
+            "connection_info": {
+                "host": "127.0.0.1",
+                "port": 3306,
+                "user": "mariadb",
+                "password": "password",
+                "database": "test"
+            },
+        }
+        with open(os.path.join(to_directory, "config.json"), "w") as f:
+            f.write(json.dumps(config, indent=2))
+            f.write("\n")
--- tests/chronovoyage/domain/test_init.py
+++ tests/chronovoyage/domain/test_init.py
@@ -21,3 +21,25 @@ class TestInitDomain:
         # when
         with pytest.raises(InitDomainError):
             InitDomain(target_dir, logger=self.logger)
+
+    def test_execute(self) -> None:
+        # when
+        dirname = "sample"
+        InitDomain(TEST_TEMP_DIR, logger=self.logger).execute(dirname)
+        # then
+        assert os.listdir(os.path.join(TEST_TEMP_DIR, dirname)) == ["config.json"]
+        with open(os.path.join(TEST_TEMP_DIR, dirname, "config.json"), "r") as f:
+            config = f.read()
+        assert config == """
+{
+  "$schema": "https://raw.githubusercontent.com/noritakaIzumi/chronovoyage/main/schema/config.schema.json",
+  "vendor": "mariadb",
+  "connection_info": {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "mariadb",
+    "password": "password",
+    "database": "test"
+  }
+}
+""".strip() + "\n"
